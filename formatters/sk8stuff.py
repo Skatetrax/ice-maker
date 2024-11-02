c5bfbe03f63e0d459a59cbcd99f8053c5fa709bf
Ashley Young <asyoung@redhat.com>
2024-11-02 00:04:44 -0400

    incremental commit for the sk8stuff formatter - abbr->full

--- formatters/sk8stuff.py
+++ formatters/sk8stuff.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import usaddress
 import numpy as np
+from utils import common
 
 
 def address_formatter(x):
@@ -31,6 +32,7 @@ def process_sk8stuff():
 
     # drop any obvious dupes, they're going to happen
     # and apply some normalization to the address section
+    df['street'] = df['street'].map(common.country_us._lookup_words)
     df = df.drop_duplicates()
     df['street'] = df['street'].apply(address_formatter)
     df['street'] = df.apply(lambda row: row.street['street'], axis=1)
